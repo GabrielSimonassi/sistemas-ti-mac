ed359106ec505350935c6de5d099ba7a7a645e44
Gabriel Simonassi <gabriel.simonassi@hotmail.com>
2024-01-18 14:38:39 -0300

    [bug] Problema de CORS na requisição

--- src/consultaMac.js
+++ src/consultaMac.js
@@ -1,15 +1,12 @@
 //import axios from 'axios';
 
+const URL = 'https://macvendors.co/api/';
 
 async function checkMac(){
     const input = document.getElementById("macAddr");
     const mac = input.value;
     console.log("Mac consultado: " + mac);
-    const info = await axios.get('https://api.macvendors.com/' + mac, {
-        headers: {
-            'Acess-Control-Allow-Origin': 'true'
-        }
-    });
+    const info = await axios.get(URL + mac);
     console.log(info);
     input.value = "";
 }
